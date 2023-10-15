c4fb5684fdb92b5b4b6e745cb331983c3e369149
jogichennaiah <chennaiahjogi394@gmail.com>
2023-10-15 14:35:38 +0530

    Auto push

--- cluster.tf
+++ cluster.tf
@@ -1,6 +1,6 @@
 # Creates Elastic cache cluster
 resource "aws_elasticache_cluster" "redis" {
-  cluster_id           = "roboshop-{var.ENV}-redis"
+  cluster_id           = "roboshop-${var.ENV}-redis"
   engine               = "redis"
   node_type            = "cache.t3.small"
   num_cache_nodes      = 1
