9e80c188b3380f688d2f507e1c23c85ef884e407
jogichennaiah <chennaiahjogi394@gmail.com>
2023-10-15 14:12:48 +0530

    Auto push

--- cluster.tf
+++ cluster.tf
@@ -50,7 +50,7 @@ resource "aws_elasticache_parameter_group" "default" {
 #}
 
 resource "aws_elasticache_subnet_group" "redis" {
-  name         = roboshop-$(var.ENV)-redis-subnet-group
+  name         = "roboshop-$(var.ENV)-redis-subnet-group"
   subnet_ids   = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS
   tags = {
     Name = "roboshop-${var.ENV}-redis-subnet-group"
