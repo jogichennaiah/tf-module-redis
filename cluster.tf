dc4b9ab9bc89eda01f04c45156fcb4c1bf0059c6
jogichennaiah <chennaiahjogi394@gmail.com>
2023-11-07 00:06:59 +0530

    Auto push

--- cluster.tf
+++ cluster.tf
@@ -1,4 +1,4 @@
-# Creates Elastic cache cluster
+# Creates Elastic Cache Cluster
 resource "aws_elasticache_cluster" "redis" {
   cluster_id           = "roboshop-${var.ENV}-redis"
   engine               = "redis"
@@ -6,7 +6,7 @@ resource "aws_elasticache_cluster" "redis" {
   num_cache_nodes      = var.REDIS_INSTANCE_COUNT
   parameter_group_name = aws_elasticache_parameter_group.default.name
   subnet_group_name    = aws_elasticache_subnet_group.redis.name
-  security_group_ids   = [aws_security_group.allows.redis.id] 
+  security_group_ids   = [aws_security_group.allows_redis.id]
   engine_version       = var.REDIS_ENGINE_VERSION
   port                 = var.REDIS_PORT
 }
@@ -17,11 +17,9 @@ resource "aws_elasticache_parameter_group" "default" {
 }
 
 resource "aws_elasticache_subnet_group" "redis" {
-  name         = "roboshop-${var.ENV}-redis-subnet-group"
-  subnet_ids   = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS
+  name       = "roboshop-${var.ENV}-redis-subnet-group"
+  subnet_ids = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS 
   tags = {
     Name = "roboshop-${var.ENV}-redis-subnet-group"
   }
-
 }
-
