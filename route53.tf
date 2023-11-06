4a1dd1176ade015827321bf0dfef223823ee5acf
jogichennaiah <chennaiahjogi394@gmail.com>
2023-11-06 23:01:30 +0530

    Auto push

--- route53.tf
+++ route53.tf
@@ -3,6 +3,9 @@
 #    name    = "redis.$(var.ENA).$(data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_NAME)"
 #    type    = "CNAME"
 #    ttl     = 10
-#    records  = [aws_docdb_cluster.docdb.endpoint] 
+#    records  = [aws_elasticache_cluster.redis.cache_nodes.0.address] 
 #}
 
+#output "redis"  {
+#    value  = aws_elasticache_cluster.redis
+#}
