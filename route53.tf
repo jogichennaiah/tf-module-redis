af0c3da9bb96b76a86a691fdb666eb7a2d3d7030
jogichennaiah <chennaiahjogi394@gmail.com>
2023-12-19 11:28:35 +0530

    Auto push

--- route53.tf
+++ route53.tf
@@ -1,11 +1,11 @@
-resource  "aws_route53_record" "redis_dns" {
-   zone_id = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_ID
-   name    = "redis.$(var.ENA).$(data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_NAME)"
-   type    = "CNAME"
-   ttl     = 10
-   records  = [aws_elasticache_cluster.redis.cache_nodes.0.address] 
+resource "aws_route53_record" "redis_dns" {
+  zone_id = data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTED_ZONE_ID 
+  name    = "redis-${var.ENV}.${data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTED_ZONE_NAME}"
+  type    = "CNAME"
+  ttl     = 10
+  records = [aws_elasticache_cluster.redis.cache_nodes.0.address]
 }
 
-output "redis"  {
-   value  = aws_elasticache_cluster.redis
-}
+# output "redis" {
+#     value   = aws_elasticache_cluster.redis
+# }
